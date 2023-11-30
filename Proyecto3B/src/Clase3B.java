1d8b477c38416d439d26914d2b0674329768bcbb
VictoriaaGG <151050555+VictoriaaGG@users.noreply.github.com>
2023-11-30 16:19:49 +0100

    Update Clase3B.java

--- Proyecto3B/src/Clase3B.java
+++ Proyecto3B/src/Clase3B.java
@@ -3,7 +3,8 @@ public class Clase3B {
 
 	public static void main(String[] args) {
 		System.out.println("Victoria Gordillo Gallardo");
-
+		System.out.println("Estoy llegando al final.");
+		
 	}
 
 }
